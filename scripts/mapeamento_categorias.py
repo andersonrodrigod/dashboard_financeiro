7c3ae0bb59803528c03dec25cb63dfae9fa85af8
andersonrodrigod <andersonhav@gmail.com>
2025-05-20 14:20:17 -0300

    filtrando dados

--- scripts/mapeamento_categorias.py
+++ scripts/mapeamento_categorias.py
@@ -2,13 +2,22 @@ import pandas as pd
 import os
 from leitura_dados import carregar_dados
 
-def filtrar_dados(df, campo=None, valor=None, data_inicio=None, data_fim=None):
-    if campo and valor:
-        df = df[df[campo] == valor]
+def filtrar_dados(df, filtros, valor=None, data_inicio=None, data_fim=None):
+    for campo, valor in filtros.items():
+        if isinstance(valor, list):
+            df = df[df[campo].isin(valor)]
+            print(f"🔍 Filtrando '{campo}' por múltiplos valores: {valor}")
+        else:
+            df = df[df[campo] == valor]
+            print(f"🔍 Filtrando '{campo}' por valor único: {valor}")
+
     if data_inicio:
-        df = df[df["Data"] >= pd.to_datetime(data_inicio)]
+        df = df[df["Data"] >= pd.to_datetime(data_inicio)] 
+        print(df["Data"].dtype) 
+
     if data_fim:
         df = df[df["Data"] <= pd.to_datetime(data_fim)]
+
     return df
 
     
@@ -16,10 +25,13 @@ caminho_arquivo = "dados/lancamentos_empresariais.csv"
 
 df = carregar_dados(caminho_arquivo)
 
-campo = "Tipo"
-valor = "Receita"
+data_inicio = "2023-01-14"
 
-resultado = filtrar_dados(df, campo, valor)
+filtros = {
+    "Tipo": "Receita",
+}
+
+resultado = filtrar_dados(df, filtros, data_inicio)
 
 
 print(resultado)
